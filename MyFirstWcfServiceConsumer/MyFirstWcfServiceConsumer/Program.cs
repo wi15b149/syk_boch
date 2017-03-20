c776ef75fef8104ab820d9dbbd57fdfe3ac31693
mkb <wi15b149@technikum-wien.at>
2017-03-20 14:03:35 +0100

    Consumer fertiggestellt

--- MyFirstWcfServiceConsumer/MyFirstWcfServiceConsumer/Program.cs
+++ MyFirstWcfServiceConsumer/MyFirstWcfServiceConsumer/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MyFirstWcfServiceConsumer.SR_SimpleWcfService;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,27 @@ namespace MyFirstWcfServiceConsumer
 {
     class Program
     {
+        private static bool run = true;
         static void Main(string[] args)
         {
+            Console.WriteLine("Lets consume a service!");
+            SimpleWcfServiceProviderClient client = new SimpleWcfServiceProviderClient();
+            client.DoWorkCompleted += client_DoWorkCompleted;
+            client.DoWorkAsync(new ChatMessage() { Header = "Testnachricht", Date = DateTime.Now, Content = "Hello World" } );
+
+            while (run)
+            {
+                Console.WriteLine("I am waiting...");
+
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void client_DoWorkCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            run = false;
+            Console.WriteLine("Data sent to Service Provider");
         }
     }
 }
