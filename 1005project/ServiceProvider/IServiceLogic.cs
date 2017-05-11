dcd36b3ffa544bf079e525d4610b43218859af50
mkb <wi15b149@technikum-wien.at>
2017-05-11 08:50:47 +0200

    Vorbereitung Prüfung V

---
+++ 1005project/ServiceProvider/IServiceLogic.cs
@@ -0,0 +1,21 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceProvider
+{
+    // HINWEIS: Mit dem Befehl "Umbenennen" im Menü "Umgestalten" können Sie den Schnittstellennamen "IServiceLogic" sowohl im Code als auch in der Konfigurationsdatei ändern.
+    [ServiceContract]
+    public interface IServiceLogic
+    {
+        [OperationContract]
+        List<Flight> QueryFlights();
+
+        [OperationContract]
+        int AddFlight(string no, string sp, string ep, string op);
+    }
+}
---
+++ 1005project/ServiceProvider/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceProvider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost sh = new ServiceHost(typeof(ServiceLogic));
+            sh.Open();
+            Console.WriteLine("Service started...");
+            Console.ReadLine();
+        }
+    }
+}
