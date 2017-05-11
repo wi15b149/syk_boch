0d98c2c1205e245a6f18dd78d653e278d0887d76
mkb <wi15b149@technikum-wien.at>
2017-05-11 08:51:08 +0200

    Vorbereitung Prüfung VI

---
+++ FirstMEF/FirstMEF/Add.cs
@@ -0,0 +1,20 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstMEF
+{
+    [Export(typeof(IOperation))]
+    [ExportMetadata("Operation", '+')]
+    class Add : IOperation
+    {
+        public int operate(int left, int right)
+        {
+            return left + right;
+        }
+    }
+}
---
+++ FirstMEF/Shared/IOperationMetaData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public interface IOperationMetaData
+    {
+        char Operation { get; }
+    }
+}
